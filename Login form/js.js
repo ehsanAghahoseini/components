7ef8a949c9e4b63de29f7d92e4471df6317cad13
ehsanAghahoseini <dacke24h@gmail.com>
2024-01-20 01:06:47 +0330

    auth form

--- Login form/js.js
+++ Login form/js.js
@@ -1,15 +1,59 @@
-const handelSelectImage = (value) => {
-    let beforeSelect = document.getElementById('beforeSelect');
-    let selected = document.getElementById('selected');
-    let selectedImage = document.getElementById('selectedImage');
-    beforeSelect.style.display = 'none'
-    selected.style.display = 'flex'
-    selectedImage.src = window.URL.createObjectURL(value.files[0])
-}
+const handelForm = (type) => {
+    let btnLogin = document.getElementById('btnLogin')
+    let btnRegister = document.getElementById('btnRegister')
+    let coverRegister = document.getElementById('coverRegister')
+    let coverLogin = document.getElementById('coverLogin')
+    let registerForm = document.getElementById('registerForm')
+    let loginForm = document.getElementById('loginForm')
+    
+    if (type == 'login') {
+        btnLogin.classList.add('bg-green-500')
+        btnLogin.classList.add('text-white')
+        btnRegister.classList.remove('bg-green-500')
+        btnRegister.classList.remove('text-white')
 
-const handelRemove = () => {
-    let beforeSelect = document.getElementById('beforeSelect');
-    let selected = document.getElementById('selected');
-    beforeSelect.style.display = 'flex'
-    selected.style.display = 'none'
+        coverLogin.classList.add('z-[1]')
+        coverLogin.classList.add('top-0')
+        coverLogin.classList.add('opacity-100')
+        coverLogin.classList.remove('z-[-1]')
+        coverLogin.classList.remove('top-[-20px]')
+        coverLogin.classList.remove('opacity-0')
+
+        coverRegister.classList.remove('z-[1]')
+        coverRegister.classList.remove('top-0')
+        coverRegister.classList.remove('opacity-100')
+        coverRegister.classList.add('z-[-1]')
+        coverRegister.classList.add('top-[-20px]')
+        coverRegister.classList.add('opacity-0')
+
+        loginForm.classList.add('flex')
+        loginForm.classList.remove('hidden')
+        registerForm.classList.add('hidden')
+        registerForm.classList.remove('flex')
+    }
+    else {
+        btnRegister.classList.add('bg-green-500')
+        btnLogin.classList.remove('text-white')
+        btnLogin.classList.remove('bg-green-500')
+        btnRegister.classList.add('text-white')
+
+        coverRegister.classList.add('z-[1]')
+        coverRegister.classList.add('top-0')
+        coverRegister.classList.add('opacity-100')
+        coverRegister.classList.remove('z-[-1]')
+        coverRegister.classList.remove('top-[-20px]')
+        coverRegister.classList.remove('opacity-0')
+
+        coverLogin.classList.remove('z-[1]')
+        coverLogin.classList.remove('top-0')
+        coverLogin.classList.remove('opacity-100')
+        coverLogin.classList.add('z-[-1]')
+        coverLogin.classList.add('top-[-20px]')
+        coverLogin.classList.add('opacity-0')
+
+        registerForm.classList.add('flex')
+        registerForm.classList.remove('hidden')
+        loginForm.classList.add('hidden')
+        loginForm.classList.remove('flex')
+    }
 }

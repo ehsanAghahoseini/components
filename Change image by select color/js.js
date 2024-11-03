c5ce40a8e8c46a635a037736ffdb9eec38589861
Ehsan <dacke24h@gmail.com>
2024-11-03 04:02:43 -0800

    select color

---
+++ Change image by select color/js.js
@@ -0,0 +1,13 @@
+const handelImage = (color) => {
+    console.log(color);
+    
+    let data = {
+        'tomato': './1.png',
+        'gray': './2.png',
+        'blue': './3.png',
+    }
+    const img = document.getElementById('image')
+    img.src = data[color]
+ 
+}
+
